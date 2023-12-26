cf0ef82ee797e3eb0ad3461eaeebf6d90f7f45ae
hardeep1666 <hardeep9981533448@gmail.com>
2023-12-26 14:41:56 +0530

    second commit

--- src/main/java/com/basicSecurityExample/BasicSecurityExampleApplication.java
+++ src/main/java/com/basicSecurityExample/BasicSecurityExampleApplication.java
@@ -3,12 +3,13 @@ package com.basicSecurityExample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-
+//main 
 @SpringBootApplication //(exclude = {SecurityAutoConfiguration.class})
 public class BasicSecurityExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasicSecurityExampleApplication.class, args);
+		
 	}
 
 }
